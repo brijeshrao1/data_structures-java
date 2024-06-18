9e825377e31294b0e70928861e07a128b14abce9
brijesh <brijeshrao333@gmail.com>
2024-06-19 00:31:32 +0530

    leetcode solns

--- linked-list-interview/2_AddTwoNumsInLL.java
+++ linked-list-interview/2_AddTwoNumsInLL.java
@@ -1,4 +1,11 @@
 //Problem: Add Two Numbers in Linked list 
+/* Explaination */
+/*
+ * Take two pointers and traverse through the both ll's. 
+ * create dummy node to hold the resultant LL.
+ * create two variables carry and sum which will hold the values. current Node to traverse through the new LL.
+ *NOTE: Whenever you have to create the new ll. In which you want to store the result of sum of ll's. Always use dummy node
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         

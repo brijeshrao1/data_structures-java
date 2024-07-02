fbb5507aec0507542fc41db47bd78782009da503
brijesh <brijeshrao333@gmail.com>
2024-07-03 02:44:34 +0530

    Detect loop in ll

--- linked-list-interview/10_Loop_or_cycle_in_LL.java
+++ linked-list-interview/10_Loop_or_cycle_in_LL.java
@@ -12,7 +12,13 @@
  * }
  */
 
-
+// #1 Bruteforce (Using Hashset)
+/*Explaination*/
+/* 
+ - Traverse through the ll and keep pushing the nodes in hashMap.
+ - while pushing we will check wether this node is already present in the node or not.
+ - if node is already there then return true else return false
+*/
 public class Solution {
     public boolean hasCycle(ListNode head) {
 
@@ -27,5 +33,29 @@ public class Solution {
             temp = temp.next;
         }
     return false;
+    }
+}
+
+// #2 Optimized Approach (Using Fast and Slow pointer)
+/*Explaination*/
+/* 
+ - Take two pointer (Slow and Fast) pointing to the head of linked list.
+ - Move the pointer util fast != null and fast.next != null
+ - return true if both pointer points to the same node 
+*/
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) return true;
+        } 
+        
+        return false;
     }
 }

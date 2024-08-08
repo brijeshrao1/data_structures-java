0c7b041550d3f6fa2d2c89f88f6ee2cc826c2a04
brijesh <brijeshrao333@gmail.com>
2024-08-09 03:02:50 +0530

    Remove duplicates from array

---
+++ Array-Interview/06_Remove_Dups_from_array.java
@@ -0,0 +1,53 @@
+//Remove duplicates from array in-place
+
+// #1 Bruteforce (Using Hash Set)
+/*Explaination*/
+/* 
+	- Traverse throught the given sorted array and keep putting all the unique(***one element only once***) elements into the sets.
+    - Now traverse through the set and put back all elements from set to the arrays and return the new resultant array.
+    - TC = nlogn + O(n)
+    - SC = O(n)
+*/
+import java.util.HashSet; 
+public class Solution {
+    public static int removeDuplicates(int[] arr,int n) {
+        // Write your code here.
+
+       HashSet < Integer > set = new HashSet < > ();
+        
+        for(int i = 0; i <  n; i++)
+        {
+            set.add(arr[i]);
+        }
+
+        int index = 0;
+        int length = set.size();
+        
+        for(int i : set)
+        {
+            arr[index++] = i;
+        }
+    return length;
+    }
+}
+
+// #2 Optimized (Two Pointer)
+/*Explaination*/
+/* 
+
+*/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int j = 1; j < nums.length; j++)
+        {
+            if(nums[i] != nums[j])
+            {
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+       return i+1; 
+    }
+}

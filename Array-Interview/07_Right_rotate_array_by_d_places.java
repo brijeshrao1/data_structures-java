40049423a9314d577456f79886588c6b325259e9
brijesh <brijeshrao333@gmail.com>
2024-08-15 23:32:37 +0530

    Right rotate array by d places

---
+++ Array-Interview/07_Right_rotate_array_by_d_places.java
@@ -0,0 +1,50 @@
+//Right rotate an array by d placess
+
+
+// #1 Bruteforce ()
+/*Explaination*/
+/* 
+	-
+	-
+	-
+*/
+
+
+// #2 Optimized ()
+/*Explaination*/
+/* 
+	-
+	-
+	-
+*/
+
+// #3 Optimal Soln()
+/*Explanation*/
+/*
+	-
+	-
+	-
+*/
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int offset = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, offset - 1);
+        reverse(nums, offset, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

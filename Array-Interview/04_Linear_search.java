84d5ccc791022d3b6386b1323958f66cc3c62183
brijesh <brijeshrao333@gmail.com>
2024-08-16 04:26:17 +0530

    Linear search in sorted array

---
+++ Array-Interview/04_Linear_search.java
@@ -0,0 +1,26 @@
+//Linear Search in sorted array
+
+// #1 Bruteforce ()
+/*Algorithm*/
+/* 
+	- Traverese throught the array and compare the K with all elements of array 
+	- if you found K then return 1 else -1
+
+	-Complexity Analysis
+	  -Time complexity  : O(n)
+      -Space complexity : O(1)
+*/
+class Solution {
+    static int searchInSorted(int arr[], int N, int K) {
+
+        // Your code here
+        for(int i = 0; i < N; i++)
+        {
+            if(arr[i] == K)
+            {
+                return 1;
+            }
+        }
+        return -1;
+    }
+}
